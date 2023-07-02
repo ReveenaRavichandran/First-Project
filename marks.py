d37ea952ac64880a2b9d7d9f1b0ef84f2df16c8a
Reveena <118819361+ReveenaRavichandran@users.noreply.github.com>
2023-07-02 18:26:47 +0530

    vote .py

--- marks.py
+++ marks.py
@@ -1,2 +1,32 @@
-s='test123string'
+import datetime
+class Student:
+    def __init__(self,regno,name,dob,age,email,vote):
+        self.regno=regno
+        self.name=name
+        self.dob=dob
+        self.age=age
+        self.email=email
+        self.vote=vote
+    def __str__(self):
+        return f'{self.regno}\t{self.name}\t{self.dob}\t\t{self.age}\t{self.email}\t{self.vote}'
+#     
+s=[]
+n=int(input("enter n:"))
+for i in range(n):
+    regno=int(input("enter regno"))
+    name=input("enter name")
+    dob=input("enter dob")
+    d=datetime.datetime.now()
+    age=int(d.strftime('%y'))-int(dob[9::])
+    if age>=18:
+        vote="yes"
+    else:
+        vote="no"
+
+    email=str(regno)+"itech.edu"
+    s.append(Student(regno,name,dob,age,email,vote))
+    print=("regno",\t name","\t dob","\t age","\t\t email","\t eligible for vote")   
+for i in s:
+    print(i)
+        
 
